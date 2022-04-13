d1c680021bfb7b4f21adf53e6136b6d8eb2411bc
Bogdan Calapod <bogdan.marius.calapod@outlook.com>
2022-04-13 08:32:53 +0100

    task(jira): Update template

--- jiraChangelog/index.js
+++ jiraChangelog/index.js
@@ -1,36 +1,36 @@
-const core = require("@actions/core");
-const _ = require("lodash");
-const Entities = require("html-entities");
-const ejs = require("ejs");
-const { SourceControl, Jira } = require("jira-changelog");
-const RegExpFromString = require("regexp-from-string");
+const core = require('@actions/core')
+const _ = require('lodash')
+const Entities = require('html-entities')
+const ejs = require('ejs')
+const { SourceControl, Jira } = require('jira-changelog')
+const RegExpFromString = require('regexp-from-string')
 
 const config = {
   jira: {
     api: {
-      host: core.getInput("jira_host"),
-      email: core.getInput("jira_email"),
-      token: core.getInput("jira_token"),
+      host: core.getInput('jira_host'),
+      email: core.getInput('jira_email'),
+      token: core.getInput('jira_token'),
     },
-    baseUrl: core.getInput("jira_base_url"),
-    ticketIDPattern: RegExpFromString(core.getInput("jira_ticket_id_pattern")),
+    baseUrl: core.getInput('jira_base_url'),
+    ticketIDPattern: RegExpFromString(core.getInput('jira_ticket_id_pattern')),
     approvalStatus: core
-      .getInput("approval_statuses")
-      .split(",")
-      .filter((x) => x !== ""),
+      .getInput('approval_statuses')
+      .split(',')
+      .filter((x) => x !== ''),
     excludeIssueTypes: core
-      .getInput("exclude_issue_types")
-      .split(",")
-      .filter((x) => x !== ""),
+      .getInput('exclude_issue_types')
+      .split(',')
+      .filter((x) => x !== ''),
     includeIssueTypes: [],
   },
   sourceControl: {
     defaultRange: {
-      from: core.getInput("source_control_range_from"),
-      to: core.getInput("source_control_range_to"),
+      from: core.getInput('source_control_range_from'),
+      to: core.getInput('source_control_range_to'),
     },
   },
-};
+}
 
 const template = `
 <% if (jira.releaseVersions && jira.releaseVersions.length) {  %>
@@ -40,7 +40,7 @@ Release version: <%= jira.releaseVersions[0].name -%>
 Jira Tickets
 ---------------------
 <% tickets.all.forEach((ticket) => { %>
-  * ![](<%= ticket.fields.issuetype.iconUrl %> "<%= ticket.fields.issuetype.name %>") **<%= ticket.fields.issuetype.name %>** [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) <% ticket.fields.components && ticket.fields.components.length > 0 && ticket.fields.components.forEach((component) => { %> **<%= component.name %>**  <% }) %>  <%= ticket.fields.summary -%>
+  * ![](<%= ticket.fields.issuetype.iconUrl %> "<%= ticket.fields.issuetype.name %>") **<%= ticket.fields.issuetype.name %>** [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) <% ticket.fields.components && ticket.fields.components.length > 0 && ticket.fields.components.map((component) => { %> **<%= component.name %>**  <% }).join(', ') %>  <%= ticket.fields.summary -%>
 <% }); -%>
 <% if (!tickets.all.length) {%> No JIRA tickets present in this release <% } %>
 
@@ -51,7 +51,7 @@ Other Commits
   * <%= commit.authorName %> - [<%= commit.revision.substr(0, 7) %>] - <%= commit.summary -%>
 <% }); -%>
 <% } %>
-`;
+`
 
 const trimmedTemplate = `
 <% if (jira.releaseVersions && jira.releaseVersions.length) {  %>
@@ -61,62 +61,59 @@ Release version: <%= jira.releaseVersions[0].name -%>
 **Jira Tickets**
 
 <% tickets.all.forEach((ticket) => { %>
-  * **<%= ticket.fields.issuetype.name %>** [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) <% ticket.fields.components && ticket.fields.components.length > 0 && ticket.fields.components.forEach((component) => { %> - <%= component.name %> -  <% }) %>  <%= ticket.fields.summary -%>
+  * **<%= ticket.fields.issuetype.name %>** [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) <% ticket.fields.components && ticket.fields.components.length > 0 && ticket.fields.components.map((component) => { %> <%= component.name %> <% }).join(', ') %>  <%= ticket.fields.summary -%>
 <% }); -%>
 <% if (!tickets.all.length) {%> No JIRA tickets present in this release <% } %>
-`;
+`
 
 function generateReleaseVersionName() {
-  const hasVersion = process.env.VERSION;
+  const hasVersion = process.env.VERSION
   if (hasVersion) {
-    return process.env.VERSION;
+    return process.env.VERSION
   } else {
-    return "";
+    return ''
   }
 }
 
 function transformCommitLogs(config, logs) {
-  let approvalStatus = config.jira.approvalStatus;
+  let approvalStatus = config.jira.approvalStatus
   if (!Array.isArray(approvalStatus)) {
-    approvalStatus = [approvalStatus];
+    approvalStatus = [approvalStatus]
   }
 
   // Tickets and their commits
   const ticketHash = logs.reduce((all, log) => {
     log.tickets.forEach((ticket) => {
-      all[ticket.key] = all[ticket.key] || ticket;
-      all[ticket.key].commits = all[ticket.key].commits || [];
-      all[ticket.key].commits.push(log);
-    });
-    return all;
-  }, {});
+      all[ticket.key] = all[ticket.key] || ticket
+      all[ticket.key].commits = all[ticket.key].commits || []
+      all[ticket.key].commits.push(log)
+    })
+    return all
+  }, {})
   const ticketList = _.sortBy(
     Object.values(ticketHash),
     (ticket) => ticket.fields.issuetype.name,
-  );
+  )
   let pendingTickets = ticketList.filter(
     (ticket) => !approvalStatus.includes(ticket.fields.status.name),
-  );
+  )
 
   // Pending ticket owners and their tickets/commits
-  const reporters = {};
+  const reporters = {}
   pendingTickets.forEach((ticket) => {
-    const email = ticket.fields.reporter.emailAddress;
+    const email = ticket.fields.reporter.emailAddress
     if (!reporters[email]) {
       reporters[email] = {
         email,
         name: ticket.fields.reporter.displayName,
         slackUser: ticket.slackUser,
         tickets: [ticket],
-      };
+      }
     } else {
-      reporters[email].tickets.push(ticket);
+      reporters[email].tickets.push(ticket)
     }
-  });
-  const pendingByOwner = _.sortBy(
-    Object.values(reporters),
-    (item) => item.user,
-  );
+  })
+  const pendingByOwner = _.sortBy(Object.values(reporters), (item) => item.user)
 
   // Output filtered data
   return {
@@ -133,59 +130,59 @@ function transformCommitLogs(config, logs) {
       ),
       pending: pendingTickets,
     },
-  };
+  }
 }
 
 async function main() {
   try {
     // Get commits for a range
-    const source = new SourceControl(config);
-    const jira = new Jira(config);
+    const source = new SourceControl(config)
+    const jira = new Jira(config)
 
-    const range = config.sourceControl.defaultRange;
-    console.log(`Getting range ${range.from}...${range.to} commit logs`);
-    const commitLogs = await source.getCommitLogs("./", range);
-    console.log("Found following commit logs:");
-    console.log(commitLogs);
+    const range = config.sourceControl.defaultRange
+    console.log(`Getting range ${range.from}...${range.to} commit logs`)
+    const commitLogs = await source.getCommitLogs('./', range)
+    console.log('Found following commit logs:')
+    console.log(commitLogs)
 
-    console.log("Generating release version");
-    const release = generateReleaseVersionName();
-    console.log(`Release: ${release}`);
+    console.log('Generating release version')
+    const release = generateReleaseVersionName()
+    console.log(`Release: ${release}`)
 
-    console.log("Generating Jira changelog from commit logs");
-    const changelog = await jira.generate(commitLogs, release);
-    console.log("Changelog entry:");
-    console.log(changelog);
+    console.log('Generating Jira changelog from commit logs')
+    const changelog = await jira.generate(commitLogs, release)
+    console.log('Changelog entry:')
+    console.log(changelog)
 
-    console.log("Generating changelog message");
-    const data = await transformCommitLogs(config, changelog);
+    console.log('Generating changelog message')
+    const data = await transformCommitLogs(config, changelog)
 
     data.jira = {
       baseUrl: config.jira.baseUrl,
       releaseVersions: jira.releaseVersions,
-    };
+    }
     data.includePendingApprovalSection =
-      core.getInput("include_pending_approval_section") === "true";
+      core.getInput('include_pending_approval_section') === 'true'
 
-    const entitles = new Entities.AllHtmlEntities();
-    const changelogMessage = ejs.render(template, data);
-    const trimmedChangelogMessage = ejs.render(trimmedTemplate, data);
+    const entitles = new Entities.AllHtmlEntities()
+    const changelogMessage = ejs.render(template, data)
+    const trimmedChangelogMessage = ejs.render(trimmedTemplate, data)
 
-    console.log("Changelog message entry:");
-    console.log(entitles.decode(changelogMessage));
+    console.log('Changelog message entry:')
+    console.log(entitles.decode(changelogMessage))
 
-    core.setOutput("changelog_message", changelogMessage);
+    core.setOutput('changelog_message', changelogMessage)
 
     core.setOutput(
-      "changelog_message_trimmed",
+      'changelog_message_trimmed',
       JSON.stringify(trimmedChangelogMessage).slice(
         1,
         JSON.stringify(trimmedChangelogMessage).length - 1,
       ),
-    );
+    )
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-main();
+main()
