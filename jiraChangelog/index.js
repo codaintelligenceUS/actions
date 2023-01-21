539644b0ca5fd8b4d36b2b11f00c32bc18e8323c
Bogdan Calapod <bogdan.calapod@codaintelligence.com>
2023-01-21 19:18:53 +0200

    task: Trim commits
    These can cause extra-long messages which aren't parsed well by github
    actions

--- jiraChangelog/index.js
+++ jiraChangelog/index.js
@@ -1,37 +1,37 @@
-const core = require('@actions/core')
-const _ = require('lodash')
-const Entities = require('html-entities')
-const ejs = require('ejs')
-const { SourceControl, Jira } = require('jira-changelog')
-const RegExpFromString = require('regexp-from-string')
+const core = require("@actions/core");
+const _ = require("lodash");
+const Entities = require("html-entities");
+const ejs = require("ejs");
+const { SourceControl, Jira } = require("jira-changelog");
+const RegExpFromString = require("regexp-from-string");
 
 const config = {
   jira: {
     api: {
-      host: core.getInput('jira_host'),
-      email: core.getInput('jira_email'),
-      token: core.getInput('jira_token'),
+      host: core.getInput("jira_host"),
+      email: core.getInput("jira_email"),
+      token: core.getInput("jira_token"),
     },
-    baseUrl: core.getInput('jira_base_url'),
-    ticketIDPattern: RegExpFromString(core.getInput('jira_ticket_id_pattern')),
+    baseUrl: core.getInput("jira_base_url"),
+    ticketIDPattern: RegExpFromString(core.getInput("jira_ticket_id_pattern")),
     approvalStatus: core
-      .getInput('approval_statuses')
-      .split(',')
-      .filter((x) => x !== ''),
+      .getInput("approval_statuses")
+      .split(",")
+      .filter((x) => x !== ""),
     excludeIssueTypes: core
-      .getInput('exclude_issue_types')
-      .split(',')
-      .filter((x) => x !== ''),
+      .getInput("exclude_issue_types")
+      .split(",")
+      .filter((x) => x !== ""),
     includeIssueTypes: [],
   },
   sourceControl: {
     defaultRange: {
-      from: core.getInput('source_control_range_from'),
-      to: core.getInput('source_control_range_to'),
+      from: core.getInput("source_control_range_from"),
+      to: core.getInput("source_control_range_to"),
     },
   },
-  extraContent: core.getInput('extra_content', { required: false }),
-}
+  extraContent: core.getInput("extra_content", { required: false }),
+};
 
 const template = `
 <% if (jira.releaseVersions && jira.releaseVersions.length) {  %>
@@ -49,14 +49,7 @@ Jira Tickets
 <% }); -%>
 <% if (!tickets.approved.length) {%> No JIRA tickets present in this release <% } %>
 
-<% if (commits.noTickets.length) { %>
-Other Commits
----------------------
-<% commits.noTickets.forEach((commit) => { %>
-  * <%= commit.authorName %> - [<%= commit.revision.substr(0, 7) %>] - <%= commit.summary -%>
-<% }); -%>
-<% } %>
-`
+`;
 
 const trimmedTemplate = `
 <% if (extraContent) { %>
@@ -73,7 +66,7 @@ const trimmedTemplate = `
   * **<%= ticket.fields.issuetype.name %>** <% ticket.fields.components && ticket.fields.components.length > 0 && ticket.fields.components.map((component) => { %> <%= component.name %> <% }).join(', ') %>  <%= ticket.fields.summary -%>
 <% }); -%>
 <% if (!tickets.approved.length) {%> No JIRA tickets present in this release <% } %>
-`
+`;
 
 const trimmedTemplateTeams = `
 <% if (jira.releaseVersions && jira.releaseVersions.length) {  %>
@@ -90,57 +83,59 @@ Release version: <%= jira.releaseVersions[0].name -%>
   * **<%= ticket.fields.issuetype.name %>** [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) <% ticket.fields.components && ticket.fields.components.length > 0 && ticket.fields.components.map((component) => { %> <%= component.name %> <% }).join(', ') %>  <%= ticket.fields.summary -%>
 <% }); -%>
 <% if (!tickets.approved.length) {%> No JIRA tickets present in this release <% } %>
-`
-
+`;
 
 function generateReleaseVersionName() {
-  const hasVersion = process.env.VERSION
+  const hasVersion = process.env.VERSION;
   if (hasVersion) {
-    return process.env.VERSION
+    return process.env.VERSION;
   } else {
-    return ''
+    return "";
   }
 }
 
 function transformCommitLogs(config, logs) {
-  let approvalStatus = config.jira.approvalStatus
+  let approvalStatus = config.jira.approvalStatus;
   if (!Array.isArray(approvalStatus)) {
-    approvalStatus = [approvalStatus]
+    approvalStatus = [approvalStatus];
   }
 
   // Tickets and their commits
   const ticketHash = logs.reduce((all, log) => {
     log.tickets.forEach((ticket) => {
-      all[ticket.key] = all[ticket.key] || ticket
-      all[ticket.key].commits = all[ticket.key].commits || []
-      all[ticket.key].commits.push(log)
-    })
-    return all
-  }, {})
+      all[ticket.key] = all[ticket.key] || ticket;
+      all[ticket.key].commits = all[ticket.key].commits || [];
+      all[ticket.key].commits.push(log);
+    });
+    return all;
+  }, {});
   const ticketList = _.sortBy(
     Object.values(ticketHash),
     (ticket) => ticket.fields.issuetype.name,
-  )
+  );
   let pendingTickets = ticketList.filter(
     (ticket) => !approvalStatus.includes(ticket.fields.status.name),
-  )
+  );
 
   // Pending ticket owners and their tickets/commits
-  const reporters = {}
+  const reporters = {};
   pendingTickets.forEach((ticket) => {
-    const email = ticket.fields.reporter.emailAddress
+    const email = ticket.fields.reporter.emailAddress;
     if (!reporters[email]) {
       reporters[email] = {
         email,
         name: ticket.fields.reporter.displayName,
         slackUser: ticket.slackUser,
         tickets: [ticket],
-      }
+      };
     } else {
-      reporters[email].tickets.push(ticket)
+      reporters[email].tickets.push(ticket);
     }
-  })
-  const pendingByOwner = _.sortBy(Object.values(reporters), (item) => item.user)
+  });
+  const pendingByOwner = _.sortBy(
+    Object.values(reporters),
+    (item) => item.user,
+  );
 
   // Output filtered data
   return {
@@ -157,79 +152,88 @@ function transformCommitLogs(config, logs) {
       ),
       pending: pendingTickets,
     },
-  }
+  };
 }
 
 async function main() {
   try {
-    const currentDate = new Date().toLocaleDateString('en-GB', {
-      day : 'numeric',
-      month : 'short',
-      year : 'numeric'
-    }).split(' ').join(' ');
+    const currentDate = new Date()
+      .toLocaleDateString("en-GB", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      })
+      .split(" ")
+      .join(" ");
 
     // Get commits for a range
-    const source = new SourceControl(config)
-    const jira = new Jira(config)
+    const source = new SourceControl(config);
+    const jira = new Jira(config);
 
-    const range = config.sourceControl.defaultRange
-    console.log(`Getting range ${range.from}...${range.to} commit logs`)
-    const commitLogs = await source.getCommitLogs('./', range)
-    console.log('Found following commit logs:')
-    console.log(commitLogs)
+    const range = config.sourceControl.defaultRange;
+    console.log(`Getting range ${range.from}...${range.to} commit logs`);
+    const commitLogs = await source.getCommitLogs("./", range);
+    console.log("Found following commit logs:");
+    console.log(commitLogs);
 
-    console.log('Generating release version')
-    const release = generateReleaseVersionName()
-    console.log(`Release: ${release}`)
+    console.log("Generating release version");
+    const release = generateReleaseVersionName();
+    console.log(`Release: ${release}`);
 
-    console.log('Reading extra content, if available')
-    const hasPGPkey = config.extraContent.indexOf('-----BEGIN PGP SIGNATURE-----') > -1
+    console.log("Reading extra content, if available");
+    const hasPGPkey =
+      config.extraContent.indexOf("-----BEGIN PGP SIGNATURE-----") > -1;
 
-    const extraContent = hasPGPkey ? config.extraContent.slice(0, config.extraContent.indexOf('-----BEGIN PGP SIGNATURE-----')) : config.extraContent
-    console.log(`Extra Content: ${extraContent}`)
+    const extraContent = hasPGPkey
+      ? config.extraContent.slice(
+          0,
+          config.extraContent.indexOf("-----BEGIN PGP SIGNATURE-----"),
+        )
+      : config.extraContent;
+    console.log(`Extra Content: ${extraContent}`);
 
-    console.log('Generating Jira changelog from commit logs')
-    const changelog = await jira.generate(commitLogs, release)
-    console.log('Changelog entry:')
-    console.log(changelog)
+    console.log("Generating Jira changelog from commit logs");
+    const changelog = await jira.generate(commitLogs, release);
+    console.log("Changelog entry:");
+    console.log(changelog);
 
-    console.log('Generating changelog message')
-    const data = await transformCommitLogs(config, changelog)
+    console.log("Generating changelog message");
+    const data = await transformCommitLogs(config, changelog);
 
     data.jira = {
       baseUrl: config.jira.baseUrl,
       releaseVersions: jira.releaseVersions,
-    }
+    };
     data.includePendingApprovalSection =
-      core.getInput('include_pending_approval_section') === 'true'
-    data.extraContent = extraContent
-    data.currentDate = currentDate
+      core.getInput("include_pending_approval_section") === "true";
+    data.extraContent = extraContent;
+    data.currentDate = currentDate;
 
-    const entitles = new Entities.AllHtmlEntities()
-    const changelogMessage = ejs.render(template, data)
-    const trimmedChangelogMessage = ejs.render(trimmedTemplate, data)
-    const trimmedChangelogMessageTeams = ejs.render(trimmedTemplateTeams, data)
+    const entitles = new Entities.AllHtmlEntities();
+    const changelogMessage = ejs.render(template, data);
+    const trimmedChangelogMessage = ejs.render(trimmedTemplate, data);
+    const trimmedChangelogMessageTeams = ejs.render(trimmedTemplateTeams, data);
 
-    console.log('Changelog message entry:')
-    console.log(entitles.decode(changelogMessage))
+    console.log("Changelog message entry:");
+    console.log(entitles.decode(changelogMessage));
 
-    console.log('Changelog trimmed message entry:')
-    console.log(entitles.decode(trimmedChangelogMessage))
+    console.log("Changelog trimmed message entry:");
+    console.log(entitles.decode(trimmedChangelogMessage));
 
-    core.setOutput('changelog_message', changelogMessage)
+    core.setOutput("changelog_message", changelogMessage);
 
-    core.setOutput('changelog_message_trimmed', trimmedChangelogMessage)
+    core.setOutput("changelog_message_trimmed", trimmedChangelogMessage);
 
     core.setOutput(
-      'changelog_message_trimmed_teams',
+      "changelog_message_trimmed_teams",
       JSON.stringify(trimmedChangelogMessageTeams).slice(
         1,
         JSON.stringify(trimmedChangelogMessageTeams).length - 1,
       ),
-    )
+    );
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-main()
+main();
